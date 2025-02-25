f4746e6142f18c4f33bd69da8cf0341063aed5d7
prasadkadam1 <136068760+prasadkadam1@users.noreply.github.com>
2025-02-25 11:34:35 +0530

    tyvyi

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import  './index.css'
+// import * as THREE from 'three';
+// const scene = new THREE.Scene();
+createRoot(document.getElementById('root')).render(
+
+  <><App /></>
+
+
+)
+
